2faa62cb223ac5c0d5b33650c8c7f012f6933bec
Benjamin Asbach <benjamin.asbach@exxeta.com>
2015-11-03 18:51:30 +0100

    extend ClassDecompiler in order to use provided logic instead of implementing JarDecompiler interface

--- src/main/java/com/exxeta/oses/maven/plugin/decompiler/jd/JdJarDecompiler.java
+++ src/main/java/com/exxeta/oses/maven/plugin/decompiler/jd/JdJarDecompiler.java
@@ -17,129 +17,48 @@
 package com.exxeta.oses.maven.plugin.decompiler.jd;
 
 import com.exxeta.oses.maven.plugin.decompiler.DecompilationException;
-import com.exxeta.oses.maven.plugin.decompiler.DecompilationStatistics;
-import com.exxeta.oses.maven.plugin.decompiler.JarDecompiler;
+
 import java.io.DataInputStream;
-import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.net.URI;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.Map;
+
 import javax.inject.Named;
+
+import com.exxeta.oses.maven.plugin.decompiler.ClassDecompiler;
+
 import jd.core.Decompiler;
 import jd.core.process.DecompilerImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author <a href="mailto:benjamin.asbach@exxeta.com">Benjamin Asbach, 2015</a>
  */
 @Named
-public class JdJarDecompiler implements JarDecompiler {
-
-    private final Logger logger = LoggerFactory.getLogger(JdJarDecompiler.class);
+public class JdJarDecompiler extends ClassDecompiler {
 
     private static final CommonPreferences DEFAULT_DECOMPILE_PREFERENCES = new CommonPreferences();
 
     @Override
-    public DecompilationStatistics decompileJar(Path jarToDecompile, Path destination) throws IOException {
-        if (Files.exists(destination)) {
-            throw new FileAlreadyExistsException(destination.toString());
+    public void decompileClass(InputStream compiledClass, OutputStream decompiledClass) throws DecompilationException {
+        try (PrintStream printStream = new PrintStream(decompiledClass)) {
+            PlainTextPrinter printer = new PlainTextPrinter(DEFAULT_DECOMPILE_PREFERENCES, printStream);
+
+            PrintStream originalSystemErr = disableJdCoreErrorMessages();
+            try (DataInputStream classFileInputStream = new DataInputStream(compiledClass)) {
+                Decompiler decompiler = new DecompilerImpl();
+                decompiler.decompile(
+                        DEFAULT_DECOMPILE_PREFERENCES,
+                        new DataInputStreamLoader(classFileInputStream),
+                        printer,
+                        ""
+                );
+            } catch (Exception ex) {
+                throw new DecompilationException(ex);
+            } finally {
+                System.setErr(originalSystemErr);
+            }
         }
-        logger.debug("Decompiling {}", jarToDecompile);
-
-        FileSystem jarFileSystem = createJarFileSystem(destination);
-        return copyEntriesFromSourceZipToDestination(jarToDecompile, jarFileSystem);
-    }
-
-    private FileSystem createJarFileSystem(Path jar) throws IOException {
-        URI zipUri = URI.create("jar:file:" + jar.toUri().getPath());
-        Map<String, Object> env = new HashMap<>();
-        env.put("create", "true");
-        return FileSystems.newFileSystem(zipUri, env);
-    }
-
-    private DecompilationStatistics copyEntriesFromSourceZipToDestination(Path jarToDecompile, final FileSystem destinationZipFileSystem) throws IOException {
-        final DecompilationStatistics stats = new DecompilationStatistics();
-        final Decompiler decompiler = new DecompilerImpl();
-
-        FileSystem sourceJarFileSystem = createJarFileSystem(jarToDecompile);
-        Files.walkFileTree(sourceJarFileSystem.getPath("/"), new SimpleFileVisitor<Path>() {
-
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                Path dirInZip = destinationZipFileSystem.getPath(dir.toString());
-                if (!Files.exists(dirInZip)) {
-                    Files.createDirectory(dirInZip);
-                }
-
-                return FileVisitResult.CONTINUE;
-            }
-
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if (fileIsAClass(file)) {
-                    logger.debug("Decompiling class {}", file.getFileName());
-                    try {
-                        writeDecompiledClassToZip(file);
-                        stats.recordDecompiledClass();
-                    } catch (DecompilationException ex) {
-                        stats.recordDecompiledErrors();
-                        Path faultyDecompiledJavaClass = ex.getFaultyFile();
-                        if (Files.exists(faultyDecompiledJavaClass)) {
-                            Files.delete(faultyDecompiledJavaClass);
-                        }
-                        logger.debug("Exception during decompilation", ex);
-                    }
-                } else {
-                    logger.debug("Since {} is not a class file. Just copy it to destionation zip", file);
-                    Files.copy(file, destinationZipFileSystem.getPath(file.toString()));
-                }
-
-                return FileVisitResult.CONTINUE;
-            }
-
-            private void writeDecompiledClassToZip(Path classFile) throws IOException, DecompilationException {
-                Path javaFilePath = Paths.get(classFile.toString().replace(".class", ".java"));
-                Path javaFileInZip = destinationZipFileSystem.getPath(javaFilePath.toString());
-
-                OutputStream javaFileInZipOutputStream = Files.newOutputStream(javaFileInZip);
-                try (PrintStream printStream = new PrintStream(javaFileInZipOutputStream)) {
-                    PlainTextPrinter printer = new PlainTextPrinter(DEFAULT_DECOMPILE_PREFERENCES, printStream);
-
-                    PrintStream originalSystemErr = disableJdCoreErrorMessages();
-                    try (DataInputStream classFileInputStream = new DataInputStream(Files.newInputStream(classFile))) {
-                        decompiler.decompile(
-                                DEFAULT_DECOMPILE_PREFERENCES,
-                                new DataInputStreamLoader(classFileInputStream),
-                                printer,
-                                "");
-                    } catch (Exception ex) {
-                        throw new DecompilationException(ex, javaFileInZip);
-                    } finally {
-                        System.setErr(originalSystemErr);
-                    }
-                }
-            }
-
-            private boolean fileIsAClass(Path file) {
-                return file.getFileName().toString().endsWith(".class");
-            }
-        });
-        destinationZipFileSystem.close();
-
-        return stats;
     }
 
     private PrintStream disableJdCoreErrorMessages() {
